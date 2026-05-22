3368eac669196c94d7135296e78dab735a0d146f
Vishnu Tadipatri <vishnut@TemuMacPlus.local>
2026-05-22 19:39:19 -0400

    updated .py file

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
-import os
-from os import environ
 from os import path
 from pathlib import Path
 
 from flask import Flask, render_template
 from flask_frozen import Freezer
 
+
 template_folder = path.abspath('./wiki')
 
-main_app = Flask(__name__, template_folder=template_folder)
-#main_app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGE_URL')
-main_app.config['FREEZER_DESTINATION'] = 'public'
-main_app.config['FREEZER_RELATIVE_URLS'] = True
-main_app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
-freezer = Freezer(main_app)
+app = Flask(__name__, template_folder=template_folder)
+#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
+app.config['FREEZER_DESTINATION'] = 'public'
+app.config['FREEZER_RELATIVE_URLS'] = True
+app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
+freezer = Freezer(app)
 
-@main_app.cli.command()
+@app.cli.command()
 def freeze():
     freezer.freeze()
 
-@main_app.cli.command()
+@app.cli.command()
 def serve():
     freezer.run()
 
-@main_app.route('/')
+@app.route('/')
 def home():
-    return render_template('pages/index.html')
+    return render_template('pages/home.html')
 
-@main_app.route('/<page>')
+@app.route('/<page>')
 def pages(page):
     return render_template(str(Path('pages')) + '/' + page.lower() + '.html')
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
-    main_app.run(port=8080)
+    app.run(port=8080)
